fbb4ca112dea6d967c470768d68b8a59e9dc5e73
tunth <40729461+tunth@users.noreply.github.com>
2023-12-22 14:26:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,79 @@
-# -*- coding: utf-8 -*-
+# app3.py
+import plotly.graph_objects as go
+import numpy as np
+from numpy import log as ln
 
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-import dash
+from dash import Dash, dcc, html, Input, Output, callback
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
-import pandas as pd
+# import dash_bootstrap_components as dbc
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# app = dash.Dash(__name__)
+app = Dash(__name__)
 server = app.server
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='Welcome,'),
 
-    html.Div(children='''
-        Dash: A web application framework for Python.!!!
-    '''),
+    # html.Div(children='''
+    #     Dash: A web application framework for Python.
+    # '''),
+    html.Div([
+        "Input: ",
+        dcc.Input(id='load_input', value=1, type='text')
+    ]),
+
+    html.H1(children=''),
 
     dcc.Graph(
-        id='example-graph',
-        figure=fig
+        id='example-graph'
+        # figure=figure
+    ),    
+], style={'width': '55%', 'display': 'inline-block'}
+)
+
+@callback(
+        Output('example-graph', 'figure'),
+        [Input('load_input', 'value')]
+)
+def plotfunc(z):
+    x = np.linspace(1, 10, 100)
+    f = lambda x: 1/(ln(x)+0.1)
+    g = lambda x: 0.5*x
+    
+    if z == '': z=1
+    else: z = float(z)
+
+    figure = go.Figure()
+    figure.add_trace(
+        go.Scatter(x=x, y=f(x),
+                name='ln(x)')
     )
-])
+    figure.add_trace(
+        go.Scatter(x=x, y=g(x),
+                name='line(x)')
+    )
+    figure.add_traces(
+        go.Scatter(x=[z], y=f([z]),
+                   marker_size=20, marker_color="yellow"
+                   )
+    )
+    figure.update_layout(
+        title="Đồ thị",
+        xaxis_title="Lợi ích",
+        yaxis_title="Chi phí",
+        xaxis=dict(range=[0,10]),
+        yaxis=dict(range=[0,10]),
+        paper_bgcolor="LightSteelBlue",
+    )
+    # figure.update_layout(width=int(width))
+    figure.update_layout(width=400)
+    # print("Giá trị min cost:", min(f(x)))
+    # figure.show()
+
+    return figure
 
 if __name__ == '__main__':
     app.run_server(debug=True)
